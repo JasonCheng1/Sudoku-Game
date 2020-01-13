2b0e9888fa2d22133a2d25cf5d594268a3d04474
Jason Cheng <jasoncheng3443@gmail.com>
2020-01-12 22:30:15 -0500

    Messed with Tkinter and decided to use pygame to work on sudoku app

---
+++ settings.py
@@ -0,0 +1,13 @@
+WIDTH = 600
+HEIGHT = 600
+
+#Color
+WHITE = (255, 255, 255) #RGB
+BLACK = (0,0,0)
+BLUE = (0, 76, 153)
+# Boards
+testBoard = [[0 for x in range(9)] for x in range(9)]
+
+# Postions
+gridPos = (75, 100)
+cellSize = 50
